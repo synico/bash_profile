d0b82853579dc5442b7c34ee28651f13d6f562e4
nick@p50 <synico@qq.com>
2018-12-02 17:26:17 +0800

    D20181201

--- install/dpkg-deb.sh
+++ install/dpkg-deb.sh
@@ -2,10 +2,12 @@
 
 cd $1
 
+echo $PWD
+
 for pkg in $(ls *.deb)
 do
     echo "###### start to install $pkg ######"
-    #dpkg -i $pkg
+    dpkg -i $pkg
     echo "###### end to install $pkg ######"
 done
 
