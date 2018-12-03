b8b0e15acc910c90dab72b5b1b68745eb4c30d6f
Nick Liu <synico@gmail.com>
2018-12-03 13:29:56 +0800

    add shortcut into ./local/share/applications/

--- install/apt-install.sh
+++ install/apt-install.sh
@@ -5,6 +5,15 @@ echo "########################vim has been installed############################
 sudo apt-get -y install git
 echo "########################git has been installed###############################"
 
+sudo apt-get -y install curl
+echo "########################curl has been installed###############################"
+
+sudo apt-get -y install axel
+echo "########################axel has been installed###############################"
+
+sudo apt-get -y install openssh-server
+echo "########################sshd has been installed###############################"
+
 sudo apt-get -y install meld
 echo "########################meld has been installed###############################"
 
